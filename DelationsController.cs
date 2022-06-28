461911404ad06df4967b55a8971045e3dc9af731
bastien aubry <pma.bastien.aubry@gmail.com>
2022-06-28 21:08:02 +0200

    feat() : ajout d'endpoints de deletion.

--- DelationsController.cs
+++ DelationsController.cs
@@ -36,6 +36,10 @@ namespace Delation.io.Api
             DelationsList _list = await getJson();
             if (_list == null)
                 return NotFound("json not found");
+
+            Delation exist = _list.Delations.FirstOrDefault(d => d.Id == delation.Id);
+            if (exist != null)
+                return BadRequest("Delation given to create already exist");
             using (StreamWriter file = new StreamWriter("db.json"))
             {
                 _list.Delations.Add(delation);
@@ -44,9 +48,65 @@ namespace Delation.io.Api
             }
             return Ok();
         }
-        
-       
 
-       
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOne(string id)
+        {
+            DelationsList _list = await getJson();
+            if (_list == null)
+                return NotFound("json not found");
+            using (StreamWriter file = new StreamWriter("db.json"))
+            {
+                Delation toDelete = _list.Delations.FirstOrDefault(d => d.Id == id);
+                if(toDelete != null)
+                     _list.Delations.Remove(toDelete);
+                string jsonList = JsonConvert.SerializeObject(_list);
+                file.Write(jsonList);
+            }
+            return Ok();
+        }
+
+        [HttpDelete("DeleteAll")]
+        public async Task<IActionResult> DeleteAll()
+        {
+            DelationsList _list = await getJson();
+            if (_list == null)
+                return NotFound("json not found");
+            using (StreamWriter file = new StreamWriter("db.json"))
+            {
+                List<Delation> toDelete = _list.Delations.ToList();
+                if (toDelete != null)
+                {
+                    foreach (var del in toDelete) _list.Delations.Remove(del);
+                }
+                string jsonList = JsonConvert.SerializeObject(_list);
+                file.Write(jsonList);
+            }
+            return Ok();
+        }
+
+        [HttpDelete("deleteByName/{name}")]
+        public async Task<IActionResult> DeleteAll(string name)
+        {
+            DelationsList _list = await getJson();
+            if (_list == null)
+                return NotFound("json not found");
+            using (StreamWriter file = new StreamWriter("db.json"))
+            {
+                List<Delation> toDelete = _list.Delations.Where(d => d.name == name).ToList();
+                if (toDelete != null)
+                {
+                    foreach (var del in toDelete) _list.Delations.Remove(del);
+                }
+                string jsonList = JsonConvert.SerializeObject(_list);
+                file.Write(jsonList);
+            }
+            return Ok();
+        }
+
+
+
+
     }
 }
